9e6898d845c3b07ae5a5eb82e0222c6c8bc04a8f
328888362 <328888362@qq.com>
2019-12-01 00:56:36 +0800

    add module

---
+++ pg/src/test/java/top/xugx/PgApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xugx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
