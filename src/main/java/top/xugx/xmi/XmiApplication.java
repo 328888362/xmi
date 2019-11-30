24c151dc20a5b54eeeaed27733ae7f8e6b6eb81b
328888362 <328888362@qq.com>
2019-11-30 17:03:14 +0800

    init

---
+++ src/main/java/top/xugx/xmi/XmiApplication.java
@@ -0,0 +1,13 @@
+package top.xugx.xmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/xugx/xmi/XmiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xugx.xmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
