cf7709b76547994ab193d94053f6ca12e5b8f931
328888362 <328888362@qq.com>
2020-02-11 12:49:29 +0800

    Fix Bug

--- src/main/java/top/xugx/Test.java
+++ src/main/java/top/xugx/Test.java
@@ -1,7 +1,16 @@
 package top.xugx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args){
-
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            list.add(i);
+        }
+        list.forEach(item->{
+            System.out.println(item);
+        });
     }
 }
